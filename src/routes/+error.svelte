3e63fa93df960ce80bd977d737f2b3cf7f39e9a0
Bastian Asmussen <bastian@asmussen.tech>
2024-05-23 10:03:44 +0200

    Update head.

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,6 +3,11 @@
     import '../app.css';
 </script>
 
+<svelte:head>
+    <title>Bastian Asmussen</title>
+    <meta name="description" content="Backend Developer, and Open Source Enthusiast.">
+</svelte:head>
+
 <div class="flex flex-col items-center justify-center h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
     <h1 class="text-9xl font-bold text-red-500">{$page.status} - {$page.error.message}</h1>
     <button on:click={() => location.href = '/'} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-10">Go Home</button>
