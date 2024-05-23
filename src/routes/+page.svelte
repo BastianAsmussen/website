85242877dc348c92c3b3960e483970089b60534d
Bastian Asmussen <bastian@asmussen.tech>
2024-05-23 10:41:41 +0200

    Moved repeated title to head.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,6 @@
 	import '../app.css';
 </script>
 
-<svelte:head>
-    <title>Bastian Asmussen</title>
-    <meta name="description" content="Backend Developer, and Open Source Enthusiast.">
-</svelte:head>
-
 <h1 class="text-3xl font-bold underline">Under Construction!</h1>
 
 <style lang="postcss">
