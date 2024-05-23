034ae711716cb9e01576c0f5491d9dc57ed4b3d9
Bastian Asmussen <bastian@asmussen.tech>
2024-05-23 18:29:04 +0200

    Update coming soon text.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,10 @@
 	import '../app.css';
 </script>
 
-<h1 class="text-3xl font-bold underline">Under Construction!</h1>
+<div class="flex flex-col items-center justify-center h-screen">
+	<h1 class="text-4xl font-bold">Under Construction</h1>
+	<p class="text-lg mt-4">This page is under construction. Please check back later.</p>
+</div>
 
 <style lang="postcss">
 	:global(html) {
