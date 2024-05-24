a0bf82c4e1e64375472e1309d6e36369df8fcc02
Bastian Asmussen <bastian@asmussen.tech>
2024-05-24 15:48:24 +0200

    Remove contact form.

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,30 +1,28 @@
 <script>
 	import { page } from '$app/stores';
 	import '../app.css';
+	import SideBar from "$lib/SideBar.svelte";
 </script>
 
 <svelte:head>
 	<title>{$page.status} - {$page.error.message}</title>
 	<meta
 		name="description"
-		content="Oops, something went wrong. Please try again or contact me if the problem persists."
+		content="Oops! Something went wrong, please try again."
 	/>
 </svelte:head>
+
+<SideBar />
 
 <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-900">
 	<h1 class="text-6xl font-bold text-red-500">{$page.status} - {$page.error.message}</h1>
 	<p class="mt-4 text-xl">
-		Oops, something went wrong. Please try again or contact me if the problem persists.
+		Oops! Something went wrong, please try again.
 	</p>
 	<div class="mt-8">
 		<button
 			on:click={() => (location.href = '/')}
 			class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">Go Home</button
-		>
-		<button
-			on:click={() => (location.href = '/contact')}
-			class="ml-4 px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-			>Contact Me</button
 		>
 	</div>
 </div>
