186432319d50704071fa70d879f35eb81b03dfc9
Bastian Asmussen <bastian@asmussen.tech>
2024-05-24 19:43:12 +0200

    Add time-based greeting.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,39 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 	import SideBar from '$lib/SideBar.svelte';
+	import { onMount } from 'svelte';
+
+	let time = new Date();
+
+	$: [greeting_text, greeting_color] = getGreeting(time);
+
+	function getGreeting(time: Date): [string, string] {
+		if (time.getHours() < 12) {
+			return ["Good morning", "text-yellow-400"];
+		} else if (time.getHours() < 18) {
+			return ["Good afternoon", "text-green-400"];
+		} else {
+			return ["Good evening", "text-blue-400"];
+		}
+	}
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			time = new Date();
+		}, 1_000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
 </script>
 
 <SideBar />
 
-<main class="mx-auto p-8 my-8 max-w-4xl">
-	<h1 class="text-4xl lg:text-6xl font-extrabold text-white p-5 text-center mt-20">
-		Hello, my name is <span class="text-blue-500">Bastian</span>!
+<main class="flex flex-col items-center justify-center">
+	<h1 class="text-5xl font-extrabold text-white p-10 text-center">
+		<span class="{greeting_color}">{greeting_text}</span>, I'm Bastian Asmussen!
 	</h1>
-
 	<p class="text-lg text-gray-300 p-5 text-center">
 		I'm a software engineer who loves to build things with code and share my knowledge with others.
 	</p>
