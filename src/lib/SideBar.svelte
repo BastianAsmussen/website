2ee3904a36e9bbc071c1f6434382808c13053fe5
Bastian Asmussen <bastian@asmussen.tech>
2024-05-24 17:58:35 +0200

    Create navbar.

---
+++ src/lib/SideBar.svelte
@@ -0,0 +1,17 @@
+<nav
+	class="fixed lg:static
+ bottom-0 left-0 right-0 z-10 flex items-center justify-between p-4 bg-white dark:bg-gray-800 shadow-md dark:shadow-lg"
+>
+	<a href="/" class="flex items-center space-x-2">
+		<img src="/logo.png" alt="Logo" class="w-8 h-8" />
+		<span class="text-lg font-bold text-gray-900 dark:text-gray-100">Home</span>
+	</a>
+
+	<div class="flex items-center space-x-4">
+		<a
+			href="/projects"
+			class="text-lg font-bold text-gray-900 dark:text-gray-100 hover:text-gray-700 dark:hover:text-gray-200"
+			>Projects</a
+		>
+	</div>
+</nav>
